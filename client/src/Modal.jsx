d8721afad15a002e0279d9da3a8e74a4faeddc45
Hieu Ho <hihho@ucdavis.edu>
2020-05-05 15:56:40 -0700

    Moved inline styling to stylesheet

--- client/src/Modal.jsx
+++ client/src/Modal.jsx
@@ -1,44 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
 
-const Image = styled.span`
-  img {
-    max-width: 100px;
-    max-height: 100px;
-    margin: 0 5px;
-    cursor: pointer;
-    overflow: hidden;
-  }
-  .backdrop {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background-color: rgb(0, 0, 0, 0.3);
-  padding: 50px;
-  }
-  .modal {
-  display: block;
-  position: absolute;
-  border-radius: 5px;
-  max-width: 500px;
-  min-height: 300px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  }
-  .modalBtn {
-  border: solid white;
-  background: white;
-  padding: 8px 16px;
-  text-align: center;
-  cursor: pointer;
-  position: absolute;
-  right: 0;
-  top: 0
-  }
-`;
+const modalBtn = {
+  border: 'solid white',
+  background: 'white',
+  padding: '8px 16px',
+  textAlign: 'center',
+  cursor: 'pointer',
+  position: 'absolute',
+  right: 0,
+  top: 0,
+};
 
 const Modal = ({ picture }) => {
   const [popUp, setPopUp] = useState(false);
@@ -46,7 +17,6 @@ const Modal = ({ picture }) => {
 
   const handleClick = (e) => {
     if (outside.current.contains(e.target)) {
-      console.log('e.target: ', e.target);
       return;
     }
     setPopUp(false);
@@ -66,35 +36,33 @@ const Modal = ({ picture }) => {
 
   return (
 
-    <Image>
-      <span>
-        <img
-          ref={outside}
-          onClick={enlarge}
-          onKeyDown={enlarge}
-          src={picture}
-          alt="IssaPicture"
-        />
-        {popUp ? (
-          <div className="backdrop">
-            <img
-              className="modal"
-              src={picture}
-              alt="IssaModal"
-            />
-            <button
-              className="modalBtn"
-              type="button"
-              label="close"
-              onClick={enlarge}
-              onKeyDown={enlarge}
-            >
-              X
-            </button>
-          </div>
-        ) : null}
-      </span>
-    </Image>
+    <span>
+      <img
+        ref={outside}
+        onClick={enlarge}
+        onKeyDown={enlarge}
+        src={picture}
+        alt="IssaPicture"
+      />
+      {popUp ? (
+        <div className="backdrop">
+          <img
+            className="modal"
+            src={picture}
+            alt="IssaModal"
+          />
+          <button
+            className="modalBtn"
+            type="button"
+            label="close"
+            onClick={enlarge}
+            onKeyDown={enlarge}
+          >
+            X
+          </button>
+        </div>
+      ) : null}
+    </span>
 
   );
 };
--- client/src/Ratings.jsx
+++ client/src/Ratings.jsx
@@ -1,39 +1,4 @@
-/* eslint-disable react/self-closing-comp */
 import React from 'react';
-import styled from 'styled-components';
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  grid-gap: 10px;
-  border-bottom: 1px solid rgb(205, 205, 177);
-  .grid {
-    border-right: 1px solid gray;
-  }
-  .exp {
-    margin: 10px 20px 0 20px;
-  }
-  .section {
-    margin: 0 0 10px 20px;
-  }
-`;
-
-const ReviewBar = styled.div`
-  display: flex;
-  align-content: space-between;
-  margin: 0 50px 0 0;
-  .bg {
-    background-color: rgb(205, 205, 177);
-    border-radius: 10px;
-    width: 150px;
-    margin: 0 20px 0 20px;
-  }
-  .bg>div {
-    background-color: rgb(255,237,0);
-    height: 20px;
-    border-radius: 10px 0 0 10px;
-  }
-`;
 
 const Ratings = ({ rating }) => (
   <div>
@@ -42,108 +7,114 @@ const Ratings = ({ rating }) => (
       <div style={{ fontSize: 'large' }}>
         Overall Rating
       </div>
+
       <div>
-        <ReviewBar>
+        <div className="reviewBar">
           <div className="bg" style={{ margin: '0 20px 15px 0' }}>
-            <div style={{ width: `${(rating.average * 20)}%` }}></div>
+            <div style={{ width: `${(rating.average * 20)}%` }} />
           </div>
           {`${rating.average} (${rating.totalReviews} Reviews)`}
-        </ReviewBar>
+        </div>
       </div>
+
       <div style={{ color: 'green' }}>
         {`${rating.recommendPercent}% would recommend this product.`}
       </div>
     </div>
     <br />
 
-    <Grid>
-      <div className="Rating grid" style={{ fontSize: 'large' }}>
-        Rating
-        <div>
-          <br />
-          <ReviewBar>
-            <div>5 stars</div>
-            <div className="bg">
-              <div style={{ width: `${(rating.five / rating.totalReviews) * 100}%` }}></div>
-            </div>
-            <div>
+    <div className="gridWrapper">
 
-              {`${rating.five} Reviews`}
-            </div>
-          </ReviewBar>
-          <br />
-          <ReviewBar>
-            4 stars
-            <div className="bg">
-              <div style={{ width: `${(rating.four / rating.totalReviews) * 100}%` }}></div>
-            </div>
-            {`${rating.four} Reviews`}
-          </ReviewBar>
-          <br />
-          <ReviewBar>
-            3 stars
-            <div className="bg">
-              <div style={{ width: `${(rating.three / rating.totalReviews) * 100}%` }}></div>
-            </div>
-            {`${rating.three} Reviews`}
-          </ReviewBar>
-          <br />
-          <ReviewBar>
-            2 stars
-            <div className="bg">
-              <div style={{ width: `${(rating.two / rating.totalReviews) * 100}%` }}></div>
-            </div>
-            {`${rating.two} Reviews`}
-          </ReviewBar>
-          <br />
-          <ReviewBar>
-            1 stars
-            <div className="bg">
-              <div style={{ width: `${(rating.one / rating.totalReviews) * 100}%` }}></div>
-            </div>
-            {`${rating.one} Reviews`}
-          </ReviewBar>
-          <br />
+      <div className="rating" style={{ fontSize: 'large' }}>
+        Rating
+        <br />
+
+        <div className="reviewBar">
+          <div>5 stars</div>
+          <div className="bg">
+            <div style={{ width: `${(rating.five / rating.totalReviews) * 100}%` }} />
+          </div>
+          <div>
+            {`${rating.five} Reviews`}
+          </div>
         </div>
+        <br />
+
+        <div className="reviewBar">
+          4 stars
+          <div className="bg">
+            <div style={{ width: `${(rating.four / rating.totalReviews) * 100}%` }} />
+          </div>
+          {`${rating.four} Reviews`}
+        </div>
+        <br />
+
+        <div className="reviewBar">
+          3 stars
+          <div className="bg">
+            <div style={{ width: `${(rating.three / rating.totalReviews) * 100}%` }} />
+          </div>
+          {`${rating.three} Reviews`}
+        </div>
+        <br />
+
+        <div className="reviewBar">
+          2 stars
+          <div className="bg">
+            <div style={{ width: `${(rating.two / rating.totalReviews) * 100}%` }} />
+          </div>
+          {`${rating.two} Reviews`}
+        </div>
+        <br />
+
+        <div className="reviewBar">
+          1 stars
+          <div className="bg">
+            <div style={{ width: `${(rating.one / rating.totalReviews) * 100}%` }} />
+          </div>
+          {`${rating.one} Reviews`}
+        </div>
+        <br />
+
       </div>
 
       <div className="overallExp" style={{ fontSize: 'large' }}>
-        <div className="exp section">
+        <div className="overall exp">
           Overall Experience
         </div>
 
         <div className="exp">
           Play Experience
         </div>
-        <ReviewBar>
+        <div className="reviewBar">
           <div className="bg">
-            <div style={{ width: `${(rating.play / 4) * 20}%` }}></div>
+            <div style={{ width: `${(rating.play / 4) * 20}%` }} />
           </div>
           {`${(rating.play / 4)}`}
-        </ReviewBar>
+        </div>
 
         <div className="exp">
           Level of Difficulty
         </div>
-        <ReviewBar>
+        <div className="reviewBar">
           <div className="bg">
-            <div style={{ width: `${(rating.difficulty / 4) * 20}%` }}></div>
+            <div style={{ width: `${(rating.difficulty / 4) * 20}%` }} />
           </div>
           {`${(rating.difficulty / 4)}`}
-        </ReviewBar>
+        </div>
 
         <div className="exp">
           Value for Money
         </div>
-        <ReviewBar>
+        <div className="reviewBar">
           <div className="bg">
-            <div style={{ width: `${(rating.value / 4) * 20}%` }}></div>
+            <div style={{ width: `${(rating.value / 4) * 20}%` }} />
           </div>
           {`${(rating.value / 4)}`}
-        </ReviewBar>
+        </div>
 
       </div>
-    </Grid>
+    </div>
 
   </div>
 );
